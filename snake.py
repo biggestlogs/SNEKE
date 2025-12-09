71ee0308a9b2847df6e009d43043cbe04aa84d08
adamsjos004 <adamsjos004@warren.k12.in.us>
2025-12-09 07:17:20 -0500

    Update 1

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 import pygame, random
+from pygame.surfarray import blit_array
 
 # Initialize pygame
 pygame.init()
@@ -48,10 +49,23 @@ score_rect=(10,10)
 
 game_over_text=font.render("Game Over!", True,RED,DARKRED)
 game_over_rect=(WINDOW_WIDTH//2,WINDOW_HEIGHT//2)
+
+continue_text=font.render("Press any key to play again", True,RED,DARKRED)
+continue_rect=continue_text.get_rect()
+continue_rect=(WINDOW_WIDTH//2,WINDOW_HEIGHT//2+64)
 # Set sounds and music
+pick_up_sound=pygame.mixer.Sound('pickup_sound')
 
 # Set images (in this case, use simple rects...so just create their coordinates)
 # For a rectangle you need (top-left x, top-left y, width, height)
+apple_coord=(500,500, SNAKE_SIZE, SNAKE_SIZE)
+apple_rect=pygame.draw.rect(display_surface, RED, apple_coord)
+
+head_coord=[head_x,head_y,SNAKE_SIZE,SNAKE_SIZE]
+head_rect=pygame.draw.rect(display_surface, GREEN, head_coord)
+
+body_coords=[]
+
 
 # The main game loop
 running = True
@@ -62,22 +76,59 @@ while running:
             running = False
 
         # Move the snake
+if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            snake_dx=-1*SNAKE_SIZE
+            snake_dy=0
+            if event.key == pygame.K_RIGHT:
+                snake_dx=SNAKE_SIZE
+                snake_dy=0
+                if event.key == pygame.K_UP:
+                    snake_dx=0
+                    snake_dx=-1*SNAKE_SIZE
+                    if event.key == pygame.K_DOWN:
+                        snake_dx=0
+                        snake_dx=SNAKE_SIZE
 
     # Add the head coordinate to the first index of the body coordinate list
     # This will essentially move all the snakes body by one position in the list
+body_coords.insert(0, head_coord, )
+
+body_coords.pop()
 
     # Update the x,y position of the snakes head and make a new coordinate
-
+snake_dx=+head_x
+snake_dy=+head_y
+head_coord=(head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
     # Check for game over
+if head_rect.left < 0 or head_rect.right > WINDOW_WIDTH or head_rect.top < 0 or head_rect.bottom > WINDOW_HEIGHT or head_coord in body_coords:
+    display_surface.blit(game_over_text, game_over_rect)
+    display_surface.blit(continue_text, continue_rect)
+    pygame.display.update()
+
+is_paused=True
+while is_paused:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_paused = False
+            is_running=False
+            if event.type == pygame.KEYDOWN:
+                score=0
+
 
     # Check for collisions
+if head_rect.collidepoint(apple_rect):
+    score+=1
+    pick_up_sound.play
 
     # Update HUD
 
     # Fill the surface
-
+display_surface.fill(WHITE)
     # Blit HUD
-
+display_surface.blit(title_text, title_rect)
+display_surface.blit(score_text, score_rect)
+pygame.draw.rect(display_surface, DARKGREEN, head_coord)
     # Blit assets
 
     # Update display and tick clock
